e693755f5fe45cdf91aa350068ba58e6e89d9089
jamescho123 <jamescho@jumbosoft.com>
2024-07-05 20:29:50 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+import streamlit as st
+
+st.write("Hello WOrld")
+st.title("AI piano accompaniment")
+st.subheader("HI")
+
+st.title("Uploading Files")
+st.markdown("---")
+audio=st.file_uploader("Please upload an audio or MIDI file",type=["WAV","MP3","MP4","MID"])
+if audio is not None:
+    st.audio(audio)
+st.slider("This is a slider")
+
+import os
+from openai import OpenAI
+
+import secret
+client = OpenAI(
+    api_key=secret.api_key
+)
+
+prompt=st.text_input("what do you want to ask?")
+def search_word_information(word):
+    prompt = word
+
+    response = client.completions.create(
+        model="gpt-3.5-turbo-instruct",
+        prompt=prompt,
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.7,
+ 
+    )
+    generated_text = response.choices[0].text
+    return generated_text
+
+st.text(search_word_information(prompt))
+
